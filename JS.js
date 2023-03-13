cdda4cc879aa7f5499135266e5aa92480698d47e
CARR0T02 <mr.gabriel.chang@gmail.com>
2023-03-13 12:11:47 +0800

    Add hovering() to simulate drawing

--- JS.js
+++ JS.js
@@ -10,4 +10,10 @@ function makeGrid(dimension) {
   document.body.appendChild(container);
 }
 
+function hovering(e) {
+  e.target.classList.add("hovering");
+  setTimeout(() => {
+    e.target.classList.remove("hovering");
+  }, 1000);
+}
 makeGrid(10);
